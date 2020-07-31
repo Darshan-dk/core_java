daed941a0a73060f634fae9e3dd590e66b8b168d
darshankpoojary <darshankpoojari@gmail.com>
2020-07-31 20:44:08 +0530

    packages

---
+++ src/com/facebook/instagram/search/SerchTester.java
@@ -0,0 +1,12 @@
+package com.facebook.instagram.search;
+
+public class SerchTester {
+
+	public static void main(String[] args) {
+	com.facebook.instagram.login.User user=new com.facebook.instagram.login.User();
+	/*this is how we use when we have same
+	variable under diff package and we need access in one main method*/
+		com.facebook.instagram.search.User user1=new com.facebook.instagram.search.User();
+	}
+
+}
