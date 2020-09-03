211d7df9f2b9448e834c684a93fbdabfad99a6cc
darshankpoojary <darshankpoojari@gmail.com>
2020-09-03 11:39:36 +0530

    example

---
+++ src/loop/forloop.java
@@ -0,0 +1,14 @@
+
+
+package loop;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<15;i++)
+        System.out.printf("I love INDIA:",+i);
+			
+	}
+
+}
---
+++ src/loop/input.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class input {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter a decimal number: ");
+	
+	double number = input.nextDouble();
+
+	System.out.println("Your number is: " + number);
+	}
+
+}
