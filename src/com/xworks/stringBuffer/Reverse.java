c79841f395938d75126d5b78e857dd8ed5696040
darshankpoojary <darshankpoojari@gmail.com>
2020-09-03 11:19:45 +0530

    assignment

---
+++ src/com/xworks/stringBuffer/Reverse.java
@@ -0,0 +1,21 @@
+package com.xworks.stringBuffer;
+
+public class Reverse {
+	public static void main(String[] args) {
+		 StringBuffer sb=	new StringBuffer("darshan");
+		 System.out.println(sb.reverse());
+		 
+		 System.out.println(sb.length());
+		 
+		 char ch[]= {'a','b','c','d'};
+		 String s =new String(ch);
+		 System.out.println(s);
+		 
+		 String a[]= {"darshan","dhanush","dheeraj","deeksha"};
+		 System.out.println(a.length);
+	
+	
+
+	}
+
+}
